de4d7a460a73933ea252d6eb72efbf9c6fc375b6
Oluwagbemiga13 <daniel.rakovsky@seznam.cz>
2025-03-24 16:39:56 -0600

    Added delete user with JWT validation.

--- src/main/java/cz/oluwagbemiga/santa/be/controller/UserController.java
+++ src/main/java/cz/oluwagbemiga/santa/be/controller/UserController.java
@@ -4,6 +4,10 @@ package cz.oluwagbemiga.santa.be.controller;
 import cz.oluwagbemiga.santa.be.dto.AuthResponse;
 import cz.oluwagbemiga.santa.be.dto.UserDTO;
 import cz.oluwagbemiga.santa.be.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class UserController {
 
     private final UserService userService;
@@ -21,7 +31,10 @@ public class UserController {
         return userService.register(userDTO);
     }
 
-    // Endpoint to delete a user by id
+    @Operation(
+            summary = "An endpoint secured with JWT",
+            security = @SecurityRequirement(name = "bearerAuth")
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
